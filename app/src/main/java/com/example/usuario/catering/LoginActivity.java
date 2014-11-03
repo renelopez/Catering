a8cfa0fa080f60679ab7f9cd07abdde74aec1089
Rene Lopez Cano <rene.lopez.cano@gmail.com>
2014-11-02 21:46:58 -0600

    Added Login Screen

--- app/src/main/java/com/example/usuario/catering/LoginActivity.java
+++ app/src/main/java/com/example/usuario/catering/LoginActivity.java
@@ -4,14 +4,38 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 
 public class LoginActivity extends Activity {
+
+    EditText userNameTxt,passwordTxt;
+    Button loginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        initUI();
+        setClicks();
+
+    }
+
+    private void initUI() {
+        userNameTxt= (EditText) findViewById(R.id.username);
+        passwordTxt= (EditText) findViewById(R.id.password);
+        loginBtn= (Button) findViewById(R.id.signIn);
+    }
+
+    private void setClicks() {
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 
 
